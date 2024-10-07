9e28b731bae3458cfffa3a394b2ca0000c62021a
Simone <135957682+Simo095@users.noreply.github.com>
2024-10-07 20:09:47 +0200

    let see

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { Button, Container } from "react-bootstrap";
+import { getListFile } from "../api/server";
 
 function App() {
   //
@@ -50,15 +51,8 @@ function App() {
       <div>
         <Button
           onClick={async () => {
-            const data = await fetch(
-              `https://agne-manager.vercel.app/api/get`,
-              {
-                method: "GET",
-              }
-            );
-            if (data.ok) {
-              console.log(await data.json());
-            }
+            const data = await getListFile();
+            console.log(data);
           }}
         ></Button>
         <h1>Gestione Elementi</h1>
