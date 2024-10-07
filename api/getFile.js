33af4faa5d06bc04959455365b1f5afa8739d269
Simone <135957682+Simo095@users.noreply.github.com>
2024-10-07 20:22:39 +0200

    destination or source?

--- api/getFile.js
+++ api/getFile.js
@@ -2,7 +2,7 @@ import { list} from "@vercel/blob";
 
 export const runtime = "edge";
 
-export async function getList(request) {
+export async function GET(request) {
   const { blobs } = await list();
   return Response.json(blobs);
 }
