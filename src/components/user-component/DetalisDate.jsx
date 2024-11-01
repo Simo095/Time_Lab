8b0ea21f037a9b49fddbc110b4ad727bfb3df42a
Simone <135957682+Simo095@users.noreply.github.com>
2024-11-01 10:53:55 +0100

    30 px left

--- src/components/user-component/DetalisDate.jsx
+++ src/components/user-component/DetalisDate.jsx
@@ -8,17 +8,18 @@ const DetailsDate = ({ selectedDate, el, setSelectedDate }) => {
       className="event-container position-relative"
       style={{
         bottom: window.innerWidth < 575 ? "300px" : "inherit",
-        left: window.innerWidth < 575 ? "-37px" : "inherit",
+        left: window.innerWidth < 575 ? "-30px" : "inherit",
       }}
     >
       {selectedDate &&
-        el.schedule.map((event, i) =>
-          event.giorno === selectedDate.toLocaleDateString("it-IT") ? (
-            <Container key={el.id} className="event-card">
-              <CardHeader event={event} setSelectedDate={setSelectedDate} />
-              <CardBody event={event} i={i} el={el} />
-            </Container>
-          ) : null
+        el.schedule.map(
+          (event, i) =>
+            event.giorno === selectedDate.toLocaleDateString("it-IT") && (
+              <Container key={el.id} className="event-card">
+                <CardHeader event={event} setSelectedDate={setSelectedDate} />
+                <CardBody event={event} i={i} el={el} />
+              </Container>
+            )
         )}
     </Container>
   );
